576789ceebbb7b6176df9d0af9c0a4b71bcdb0f1
Brian DiRito <bdirito91@gmail.com>
2019-02-14 09:34:08 -0500

    added notes for easiest JS solution valid_palindrome

--- valid_palindrome/easiest.js
+++ valid_palindrome/easiest.js
@@ -7,11 +7,14 @@ function palindromeCheck(str){
         let filtStr = s.toLowerCase().match(/[a-z]/g)
         // check if any letters left
         if (filtStr){
+            // if any letters left check if the joined filtered string is the same as its reverse
             return filtStr.join("") === filtStr.reverse().join("")
         } else {
+            // if there is nothing left after filtering, return false
             return false
         }  
     } else {
+        // if an empty string was passed in, return false.
         return false
     }
 }
