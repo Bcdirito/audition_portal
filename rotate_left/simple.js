c3f8c1693888be0a2ad6348d933c466889c0e40b
Brian DiRito <bdirito91@gmail.com>
2019-02-18 11:11:27 -0500

    added notes to rotate_left simple.js

--- rotate_left/simple.js
+++ rotate_left/simple.js
@@ -1,11 +1,12 @@
 function rotLeft(a, d) {
     // get array slice that will be rotated to the end
-    // start at the beginning
+    // since "d" is the amount of rotations, slice from the first index that amount.
     let arr1 = a.slice(0, d)
 
     // get array slice that will be rotated to the beginning
+    // since "d" is the amount of rotations, start at the matching index and go to the end.
     let arr2 = a.slice(d, a.length)
     
-    // concat the two arrays into one array matching the original length
+    // concat the first array to the end of the second array to match the original length
     return arr2.concat(arr1)
 }
