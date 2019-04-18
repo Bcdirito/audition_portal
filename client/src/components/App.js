7aa3eb3c73a8c3bddeffc065dc215398e9ef4945
Brian DiRito <bdirito91@gmail.com>
2019-04-18 12:28:26 -0400

    started routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import {Route, Switch, withRouter} from "react-router-dom"
+import Home from "./Home"
+import Login from "./Login"
 
 class App extends Component {
   render() {
     return (
       <div>
-        Loaded the Page Content
+        <Switch>
+          <Route exact path="/" render={(props) => (
+            <Home {...props} />
+          )}/>
+          <Route exact path="/login" render={(props) => (
+            <Login {...props} />
+          )}/>
+        </Switch>
       </div>
     )
   }
 }
 
-export default App
+export default withRouter(App)
